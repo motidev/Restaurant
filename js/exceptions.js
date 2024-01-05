ca720af7607ca4dff18fb9510a59c6dfaa1caa18
Moti <angelnabro@gmail.com>
2024-01-05 21:33:18 +0100

    Creamos y exportamos EmptyValueException

--- js/exceptions.js
+++ js/exceptions.js
@@ -9,3 +9,18 @@ class BaseException extends Error {
     }
 }
 
+// Excepcion para variables que esten vacias
+class EmptyValueException extends BaseException {
+    constructor(fileName, fileNumber) {
+        super(
+            "[Error] => The value cannnot be empty",
+            fileName,
+            fileNumber
+        );
+        this.name = "EmptyValueException";
+    }
+}
+
+
+// Exportamos las excepciones
+export { EmptyValueException };
