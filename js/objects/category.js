d6488814b32eb1b5e220a8ea7def83c50ba254c2
Moti <angelnabro@gmail.com>
2024-01-06 23:52:25 +0100

    Añadimos la clase Category

---
+++ js/objects/category.js
@@ -0,0 +1,49 @@
+class Category { 
+    #name;
+    #description;
+
+    // Creamos el constructor que tendra 
+    // como parametro obligatorio el nombre
+    constructor(name) {
+        // Comprobamoes si no se introduce ningun valor, lanzamos excepcion
+        if (name === undefined || name === "") throw new EmptyValueException("name");
+
+        // Comprobamos que se cree con el operador new
+        if (!(new.target === Category)) throw new InvalidConstructorException("Category");
+        
+        // Declaramos las variables
+        this.#name = name;
+        this.#description = null;
+    }
+
+    // Getters
+    get name() {
+        return this.#name;
+    }
+
+    get description() {
+        return this.#description;
+    }
+
+    // Setters 
+    set name(value) {
+        // Comprobamoes si no se introduce ningun valor, lanzamos excepcion
+        if (value === undefined || value === "") throw new EmptyValueException("name");
+
+        this.#name = value;
+    }
+
+    set description(value) {
+        // Comprobamoes si no se introduce ningun valor, lanzamos excepcion
+        if (value === undefined || value === "") throw new EmptyValueException("description");
+
+        this.#description = value;
+    }
+
+    // Imprimimos por pantalla las propiedades del objeto category
+    toString() {
+        return `Category [name = ${this.#name}, description = ${this.#description}]`;
+    }
+}
+
+export { Category };
--- js/test.js
+++ js/test.js
@@ -1,26 +1,40 @@
-import { Dish } from "./objects/Dish.js";
+import { Category } from "./objects/category.js";
+import { Dish } from "./objects/dish.js";
 
 // Creamos nuestra funcion de testeo
 function test() {
     console.log("Test fucntion");
 
-    // Comprobamos la funcionalidad de nuestro objeto Dish
+    // Testeo dish
     try {
+        console.log("--- TESTEANDO CLASE DISH ---");
+
+        // Creamos el objeto dish
         const dish = new Dish("paella");
-
-        console.log(dish);
-
-        // Añadimos informacion
         dish.description = "Una delicia de arroz valenciano";
-        dish.ingredients = ["arroz", "bogabante", "judias"];
+        dish.ingredients = ["arroz", "bogabante", "pollo"];
+
+        // Imprimimos el contendio del String
+        console.log(dish.toString());
 
         // Comprobamos la funcionalidad de nuestra clase
         console.log("Dish name -> " + dish.name);
         console.log("Dish description -> " + dish.description);
-        
-
     } catch (error) {
-        console.log(error);
+        console.log(error.message);
+    }
+
+    // Testeo Category
+    try {
+        console.log("--- TESTEANDO CLASE CATEGORY ---");
+
+        // Creamos la categoria
+        const category = new Category("Arroces");
+        category.description = "Arroces de nuestra carta";
+
+        console.log(category.toString());
+    } catch (error) {
+        console.log(error.message)
     }
 }
 
